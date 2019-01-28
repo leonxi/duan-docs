2bb353d133559afc2069cb5b5937c507de3a5313
leon_xi <leon_xi@163.com>
2019-01-28 22:42:20 +0800

    更新部署Shell

---
+++ deploy-duan-stack-storage.sh
@@ -0,0 +1,2 @@
+docker stack rm duan-storage
+docker stack deploy -c docker-compose-storage.yml duan-storage
---
+++ pull-duan-stack.sh
@@ -0,0 +1,2 @@
+curl https://raw.githubusercontent.com/leonxi/duan-docs/master/docker-compose-storage.yml -o docker-compose-storage.yml
+curl https://raw.githubusercontent.com/leonxi/duan-docs/master/docker-compose.yml -o docker-compose.yml
