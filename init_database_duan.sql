9b1f14107eeb731bf666eaad32335ffd97a4c789
leon_xi <leon_xi@163.com>
2019-01-28 19:21:55 +0800

    更新 MySQL

--- init_database_duan.sql
+++
@@ -1,4 +0,0 @@
-create database duan default character set utf8 collate utf8_general_ci;
-create user 'duan'@'%' identified by '1234';
-grant select,insert,update,delete,create on duan.* to duan;
-flush  privileges;
