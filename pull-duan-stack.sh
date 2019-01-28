b40377914e15648769126136f0535dac57ac25fa
leon_xi <leon_xi@163.com>
2019-01-28 22:44:23 +0800

    Update pull-duan-stack.sh

--- pull-duan-stack.sh
+++ pull-duan-stack.sh
@@ -1,2 +1,7 @@
+#curl https://raw.githubusercontent.com/leonxi/duan-docs/master/pull-duan-stack.sh -o pull-duan-stack.sh && chmod +x pull-duan-stack.sh
+
 curl https://raw.githubusercontent.com/leonxi/duan-docs/master/docker-compose-storage.yml -o docker-compose-storage.yml
 curl https://raw.githubusercontent.com/leonxi/duan-docs/master/docker-compose.yml -o docker-compose.yml
+
+curl https://raw.githubusercontent.com/leonxi/duan-docs/master/deploy-duan-stack.sh -o deploy-duan-stack.sh && chmod +x deploy-duan-stack.sh
+curl https://raw.githubusercontent.com/leonxi/duan-docs/master/deploy-duan-stack-storage.sh -o deploy-duan-stack-storage.sh && chmod +x deploy-duan-stack-storage.sh
